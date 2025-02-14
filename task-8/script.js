6e82d12d502c620c8839569a3d9b9c1569d072ba
gopiramani1025 <gopiramani99257@gmail.com>
2025-02-14 11:33:34 +0000

    task-8

---
+++ task-8/script.js
@@ -0,0 +1,8 @@
+var menuButton = document.getElementById('hamburger');
+var menuBar = document.getElementsByClassName('menu-bar');
+console.log(menuBar)
+
+
+navButton.addEventListener( 'click',function(e){
+     menuBar[0].style.display = 'block';
+} );
